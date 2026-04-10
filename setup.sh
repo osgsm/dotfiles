41abeadb1934c9a779b9c8b06f776b1ca763f641
osgsm <50955473+osgsm@users.noreply.github.com>
2026-04-10 13:46:05 +0900

    Update filename

--- setup.sh
+++ setup.sh
@@ -6,13 +6,30 @@ xcode-select --install > /dev/null
 # Install brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" > /dev/null
 
-# Create simbolic links
-DOT_FILES=(.zshrc .gitconfig .vimrc .tmux.conf .alacritty.yml Brewfile)
+DOTFILES=$HOME/dotfiles
 
-for file in ${DOT_FILES[@]}
-do
-	ln -s $HOME/dotfiles/$file $HOME/$file
+# Create symbolic links to $HOME
+HOME_FILES=(zshrc gitconfig vimrc tmux.conf alacritty.yml wezterm.lua)
+for file in ${HOME_FILES[@]}; do
+  ln -s $DOTFILES/$file $HOME/.$file
 done
+
+# Create symbolic links to ~/.config/
+CONFIG_FILES=(
+  "ghostty:ghostty/config"
+  "kitty.conf:kitty/kitty.conf"
+  "lazygit.yml:lazygit/config.yml"
+  "cmux:cmux"
+)
+for entry in ${CONFIG_FILES[@]}; do
+  src=${entry%%:*}
+  dest=${entry#*:}
+  mkdir -p $HOME/.config/${dest%/*}
+  ln -s $DOTFILES/$src $HOME/.config/$dest
+done
+
+# Brewfile
+ln -s $DOTFILES/Brewfile $HOME/.Brewfile
 
 # Brew install
 brew bundle --global
